da63bb314e5e98a26da30499a4a2d42bc3b0fdde
Antonio Junior <antoniocarlosjuninhojuca@gmail.com>
2022-05-23 18:24:17 -0300

    Idade22

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+int idadeMin = 0;
+int idadeMax = 0;
+int MediaAluno = 0;
+
+Console.WriteLine("Digite a idade Minima");
+idadeMin = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite a idade maxima");
+idadeMax = Convert.ToInt32(Console.ReadLine());
+MediaAluno = (idadeMin + idadeMax) /2;
+
+Console.WriteLine($"A média do aluno é: {MediaAluno}");
